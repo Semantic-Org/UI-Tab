f51901b80248b903a585ce3291445575a68e7eff
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:15:05 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-tab',
+  summary : 'Semantic UI - Tab (official): Single component release of tab',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Tab.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
