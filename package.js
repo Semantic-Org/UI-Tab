6eca71ce2f8be82581026b5efa18dfdbdc6cadbf
Pusher Robot <jack@semantic-ui.com>
2015-04-26 21:14:00 -0400

    Updated component to version 1.12.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-tab',
   summary : 'Semantic UI - Tab: Single component release',
-  version : '1.12.0',
+  version : '1.12.1',
   git     : 'git://github.com/Semantic-Org/UI-Tab.git',
 });
 
